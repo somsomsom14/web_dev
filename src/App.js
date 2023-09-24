d2c3a9a6e655c3c1991aa0fdc95035c035473940
findcheese <128571878+findcheese@users.noreply.github.com>
2023-09-24 15:42:54 +0900

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 
-import AppBasic01 from './AppBasic01';
-import AppBasic02 from './AppBasic02';
-import AppBasic03 from './AppBasic03';
-import AppBasic04 from './AppBasic04';
-import AppBasic05 from './AppBasic05';
-import AppBasic06 from './AppBasic06';
-import AppBasic07 from './AppBasic07';
-import AppBasic08 from './AppBasic08';
-import AppBasic09 from './AppBasic09';
+import AppBasicmain from './AppBasicmain';
+
 
 export default function App() {
-  return <AppBasic07 />;
+  return <AppBasicmain />;
 
 }
